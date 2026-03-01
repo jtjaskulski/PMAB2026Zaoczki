383fba4ff9dfccb0fcf3f1e064bb1b9c5e2ce555
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-01 10:12:39 +0100

    feat(Lecture3): first usage of CQRS - get all items query

--- SolutionOrders.API/Features/Items/Handlers/Queries/GetAllItemsHandler.cs
+++ SolutionOrders.API/Features/Items/Handlers/Queries/GetAllItemsHandler.cs
@@ -1,6 +1,48 @@
-﻿namespace SolutionOrders.API.Features.Items.Handlers.Queries
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SolutionOrders.API.Features.Items.Messages.DTOs;
+using SolutionOrders.API.Features.Items.Messages.Queries;
+using SolutionOrders.API.Models.Data;
+
+namespace SolutionOrders.API.Features.Items.Handlers.Queries
 {
-    public class GetAllItemsHandler
+    public class GetAllItemsHandler : IRequestHandler<GetAllItemsQuery, IEnumerable<ItemDto>>
     {
+        private readonly ApplicationDbContext _context;
+
+        public GetAllItemsHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ItemDto>> Handle(GetAllItemsQuery request, CancellationToken cancellationToken)
+        {
+            // Query do bazy z Include (EAGER LOADING)
+            var items = await _context.Items
+                .AsNoTracking()                         // Tylko do odczytu
+                .Include(i => i.Category)              // JOIN z Category
+                .Include(i => i.UnitOfMeasurement)     // JOIN z UnitOfMeasurement
+                .Where(i => i.IsActive)                // Tylko aktywne
+                .OrderBy(i => i.Name)                  // Sortowanie
+                .Select(i => new ItemDto               // Projekcja do DTO
+                {
+                    IdItem = i.IdItem,
+                    Name = i.Name,
+                    Description = i.Description,
+                    IdCategory = i.IdCategory,
+                    CategoryName = i.Category.Name,
+                    Price = i.Price,
+                    Quantity = i.Quantity,
+                    IdUnitOfMeasurement = i.IdUnitOfMeasurement,
+                    UnitName = i.UnitOfMeasurement != null
+                        ? i.UnitOfMeasurement.Name
+                        : null,
+                    Code = i.Code,
+                    IsActive = i.IsActive
+                })
+                .ToListAsync(cancellationToken);
+
+            return items;
+        }
     }
 }
