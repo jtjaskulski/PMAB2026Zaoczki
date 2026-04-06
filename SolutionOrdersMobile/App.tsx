555d2373a4994566526b12a3ebdeb2a2876d53ae
Tomasz Litwinski <tomek.litwinski2002@gmail.com>
2026-04-06 18:13:31 +0200

    zmiana tekstu (test)

---
+++ SolutionOrdersMobile/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello Tomasz Litwinski test commit1!</Text>
+      <Text style={styles.subtitle}>with TypeScript 🚀</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    marginTop: 8,
+  },
+});
+
+export default App;
